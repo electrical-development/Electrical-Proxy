26a8be141e27bdabb67a6abe7a88eefb52438238
Nobodycares-lo <140128868+Nobodycares-lo@users.noreply.github.com>
2024-06-23 18:51:48 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://muddy-rice-ba72.acsessremote.workers.dev/',
+    bare: 'https://normal.playstation451nep.workers.dev',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
