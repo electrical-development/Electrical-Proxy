de97062bc2e629c21359c49789e752399bf94799
play452 <oscarlin@edu.schools.us.kg>
2024-11-15 12:27:51 -0500

    Update register-sw.js

--- assets/register-sw.js
+++ assets/register-sw.js
@@ -1,6 +1,6 @@
 // The service worker registration script
 // This must run successfully before Ultraviolet is available to use
-const BARE_SERVER = "https://bare.electrial.site"
+const BARE_SERVER = "https://bare.hyghj.eu.org"
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./uv-sw.js', {
